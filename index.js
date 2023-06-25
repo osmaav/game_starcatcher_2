19ae793de1d4e580b44278e5132e7c3892404ecb
osmmav <osmmav@ngs.ru>
2023-06-25 13:49:30 +0700

    скорректировал функцию boyMoove

--- index.js
+++ index.js
@@ -174,8 +174,8 @@ function boyMoove() {
   // } else if (boy.dx < 0) {
   //   boy.dx -= boy.gravity;
   // }
-  if (boy.dx > boy.speed + 10 || boy.dx < -boy.speed - 10) boy.dx = 0;//останавливаем движение
-  boy.x += boy.dx;//двигаем мальчика
+  if (boy.dx > boy.speed + 5 || boy.dx < -boy.speed - 5) boy.dx = 0;//останавливаем движение
+  if (!boy.dy || boy.dy <0) boy.x += boy.dx;//двигаем мальчика
 }
 //проверяю не вылетел ли мальчик за границы экрана
 function boyCheckPosition() {
